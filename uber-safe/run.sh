7b2c312ee760a4b72f8943f89a276891534ee6e1
Yan Zhai <zhaiyan920@gmail.com>
2018-04-22 19:26:13 -0500

    compile done for generated logics

--- uber-safe/run.sh
+++ uber-safe/run.sh
@@ -2,5 +2,14 @@
 
 script=`readlink -f $0`
 workdir=`dirname $script`
+RIAK_IP=localhost
+guard="0x123456"
 
-sbt "project safe-server" "run -f $workdir/safe-apps/cloud-attestation/latte.slang  -r safeService  -kd   src/main/resources/multi-principal-keys/"
+
+cd $workdir/safe-apps/cloud-attestation/latte-generator
+python3 main.py -b $guard -e endorsements/*.json -g guards/*.json > latte.slang
+cd $workdir
+sed -i "/.*url = \"http/ s:.*:    url = \"http\://${RIAK_IP}\:8098/types/safesets/buckets/safe/keys\":" safe-server/src/main/resources/application.conf
+
+
+sbt "project safe-server" "run -f $workdir/safe-apps/cloud-attestation/latte-generator/latte.slang  -r safeService  -kd   src/main/resources/multi-principal-keys/"
