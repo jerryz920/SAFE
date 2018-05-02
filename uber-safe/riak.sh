8f5f67272cc15518ee53357beeee5b735f965027
Yan Zhai <zhaiyan920@gmail.com>
2018-05-02 10:23:59 -0500

    scripts for safe and raik container

--- uber-safe/riak.sh
+++ uber-safe/riak.sh
@@ -5,7 +5,7 @@ case $1 in
     echo "stopping riak"
     docker rm -f riak
     echo "starting riak"
-    docker run -t --rm -d -p 8098:8098 --hostname=riak --name riak riak
+    docker run -t --rm -d -p 8098:8098 -p 8087:8087 --hostname=riak --name riak riak
     docker network connect devnet riak
     ;;
   stop) 
@@ -14,7 +14,7 @@ case $1 in
     ;;
   *)
     echo "starting riak"
-    docker run -t --rm -d -p 8098:8098 --hostname=riak --name riak riak
+    docker run -t --rm -d -p 8098:8098 -p 8087:8087 --hostname=riak --name riak riak
     docker network connect devnet riak
     ;;
 esac
