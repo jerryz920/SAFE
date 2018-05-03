591b901c3c65282fd3559362c16eaa0cb7f9578a
Yan Zhai <zhaiyan920@gmail.com>
2018-05-03 16:39:14 -0500

    latte used safe container

--- uber-safe/dockerfiles/safe/start.sh
+++ uber-safe/dockerfiles/safe/start.sh
@@ -5,10 +5,11 @@ cd ~/SAFE/uber-safe
 
 # Set up: configuration the storage server for SAFE
 #sed -i "/.*url = \"http/ s:.*:    url = \"http\://${RIAK_IP}\:8098/types/safesets/buckets/safe/keys\":" safe-server/src/main/resources/application.conf
+export PATH=$PATH:$HOME/sbt/bin/
 bash run.sh
 
 # Run
- ~/sbt/bin/sbt "project safe-server" "run -f /root/SAFE/uber-safe/safe-apps/cloud-attestation/latte.slang  -r safeService  -kd   src/main/resources/multi-principal-keys/" 
+#~/sbt/bin/sbt "project safe-server" "run -f /root/SAFE/uber-safe/safe-apps/cloud-attestation/latte.slang  -r safeService  -kd   src/main/resources/multi-principal-keys/" 
 
 #bash ~/test.sh
 /bin/bash
